77b829769b49f0c7c446304d7c50edcef463d300
Lahiru mirihagoda <lahiru.mirihagoda@gmail.com>
2019-05-29 23:35:26 +0530

    setup routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingComponent } from './components/landing/landing.component';
+import { AboutComponent } from './components/about/about.component';
+import { WorksComponent } from './components/works/works.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 const routes: Routes = [
   { path: '', component: LandingComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'projects', component: WorksComponent},
+  { path: 'contact', component: ContactComponent}
 ];
 
 @NgModule({
