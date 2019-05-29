f6b0d18dfb83a95bdad07a69d28985cd2ef2c539
Lahiru mirihagoda <lahiru.mirihagoda@gmail.com>
2019-05-29 22:42:33 +0530

    Initial file creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { AboutComponent } from './components/about/about.component';
+import { WorksComponent } from './components/works/works.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { LandingComponent } from './components/landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    AboutComponent,
+    WorksComponent,
+    ContactComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
